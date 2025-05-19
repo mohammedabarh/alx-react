9c3bd64ab6e0c8bd1e8be91c65c4f8bc58b854aa
mohammedabarh <abmohammed606@gmail.com>
2025-05-19 16:03:59 +0000

    task

--- 0x01-react_intro/task_4/dashboard/0x00-Webpack/task_0/src/index.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-$(document).ready(function() {
-  $('body').append('<p>ALX Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - ALX</p>');
-});
